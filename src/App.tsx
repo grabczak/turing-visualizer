52df44d6b676a3fe30fa04a0cca4d7d8c896edcb
Kacper Grabczak <kacpergrabczak@gmail.com>
2025-10-17 21:25:33 +0200

    Install redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { Table } from "src/components/Table";
 import { Tape } from "src/components/Tape";
-import { store } from "src/store";
+import { store, persistor } from "src/store";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="container mx-auto flex flex-col items-center p-4">
-        <Table />
-        <Tape />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="container mx-auto flex flex-col items-center p-4">
+          <Table />
+          <Tape />
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import tableReducer from "./table";
 import tapeReducer from "./tape";
 
-export const store = configureStore({
-  reducer: { table: tableReducer, tape: tapeReducer },
+const rootReducer = combineReducers({
+  table: tableReducer,
+  tape: tapeReducer,
 });
+
+const persistedReducer = persistReducer(
+  {
+    key: "root",
+    version: 1,
+    storage,
+    blacklist: ["tape"],
+  },
+  rootReducer,
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
